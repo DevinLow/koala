88279e4dc24270ca796680241a8fdd4eb8e51ff5
DevinLiu <gang_liu1987@163.com>
2016-06-13 15:07:24 +0800

    add test flow

--- app/src/main/java/com/ospicon/koalafinaltestapp/MainActivity.java
+++ app/src/main/java/com/ospicon/koalafinaltestapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ospicon.koalafinaltestapp;
 
-import android.bluetooth.BluetoothDevice;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.graphics.Color;
+import android.os.CountDownTimer;
 import android.os.Parcel;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -61,8 +63,8 @@ public class MainActivity extends AppCompatActivity implements KoalaInterface {
     public TextView tv_btaddr;
     public TextView tv_matfw;
     public TextView tv_btfw;
-    public TextView tv_matmodel;
-    public TextView tv_matrssi;
+//    public TextView tv_matmodel;
+//    public TextView tv_matrssi;
     public TextView tv_soundlvllow;
     public TextView tv_soundlvlhigh;
     public TextView tv_breath;
@@ -157,8 +159,8 @@ public class MainActivity extends AppCompatActivity implements KoalaInterface {
         tv_btaddr=(TextView)findViewById(R.id.tv_btaddr);
         tv_matfw=(TextView)findViewById(R.id.tv_matfw);
         tv_btfw=(TextView)findViewById(R.id.tv_btfw);
-        tv_matmodel=(TextView)findViewById(R.id.tv_matmodel);
-        tv_matrssi=(TextView)findViewById(R.id.tv_matrssi);
+//        tv_matmodel=(TextView)findViewById(R.id.tv_matmodel);
+//        tv_matrssi=(TextView)findViewById(R.id.tv_matrssi);
         tv_soundlvllow=(TextView)findViewById(R.id.tv_soundlvllow);
         tv_soundlvlhigh=(TextView)findViewById(R.id.tv_soundlvlhigh);
         tv_breath=(TextView)findViewById(R.id.tv_breath);
@@ -169,21 +171,147 @@ public class MainActivity extends AppCompatActivity implements KoalaInterface {
         tv_result=(TextView)findViewById(R.id.tv_result);
         bt_test1=(Button)findViewById(R.id.bt_test1);
         bt_test2=(Button)findViewById(R.id.bt_test2);
+        final CountDownTimer timer4 = new CountDownTimer((Integer.parseInt(configInfo.mStrOutOfMatTime)*1000),1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tv_outofmat.setText("" + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                if(mSleepState==0){
+                    tv_outofmat.setText("PASS");
+                    if(koalaSDK.resetKoala()){
+                        tv_factroyreset.setText("PASS");
+                        tv_result.setText("PASS");
+                    }else {
+                        tv_factroyreset.setText("Fail...Error9");
+                        tv_factroyreset.setTextColor(Color.RED);
+                    }
+                }else{
+                    tv_outofmat.setText("Fail...Error8");
+                    tv_outofmat.setTextColor(Color.RED);
+                }
+            }
+        };
+        final CountDownTimer timer3 = new CountDownTimer((Integer.parseInt(configInfo.mStrWaitTime)*1000),1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tv_outofmat.setText("" + millisUntilFinished / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("請移除重量");
+                builder.setTitle("提示");
+                builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+                timer4.start();
+            }
+        };
+        final CountDownTimer timer2 = new CountDownTimer((Integer.parseInt(configInfo.mStrNoBreathTime))*1000,1000){
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tv_nobreath.setText(""+millisUntilFinished/1000);
+            }
+
+            @Override
+            public void onFinish() {
+                if(mBpm<=0){
+                    tv_nobreath.setText("PASS");
+                    bt_test2.setTextColor(Color.BLACK);
+                    bt_test2.setClickable(true);
+                }else {
+                    tv_nobreath.setText("Fail...Error 8");
+                    tv_nobreath.setTextColor(Color.RED);
+                }
+            }
+
+        };
+        final CountDownTimer timer1=new CountDownTimer(Integer.parseInt(configInfo.mStrBreathCountDown)*1000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                tv_breath.setText("" + millisUntilFinished / 1000);
+            }
+            @Override
+            public void onFinish() {
+                if(mBpm<=Integer.parseInt(configInfo.mStrBreathHigh)&&mBpm>=Integer.parseInt(configInfo.mStrBreathLow)){
+                    tv_breath.setText(Integer.toString(mBpm));
+                    if(mTemperature>Integer.parseInt(configInfo.mStrTemperatureLow)&&mTemperature<Integer.parseInt(configInfo.mStrTemperatureHigh)){
+                        tv_temperature.setText(Integer.toString(mTemperature));
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setMessage("请关闭马达");
+                        builder.setTitle("提示");
+                        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                                timer2.start();
+                            }
+                        });
+                        builder.create().show();
+                    }else {
+                        tv_temperature.setText(Integer.toString(mTemperature)+"...Error 7");
+                        tv_temperature.setTextColor(Color.RED);
+                    }
+                }else {
+                    tv_breath.setText(Integer.toString(mBpm)+"...Error 7");
+                    tv_breath.setTextColor(Color.RED);
+                }
+            }
+        };
+
         bt_test1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                bt_test1.setTextColor(Color.GRAY);
+                bt_test1.setClickable(false);
+                if(mSound<Integer.parseInt(configInfo.mStrSoundLow)){
+                    tv_soundlvllow.setText(Integer.toString(mSound));
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("请打开音箱");
+                    builder.setTitle("提示");
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                            if(mSound>=Integer.parseInt(configInfo.mStrSoundHigh)){
+                                tv_soundlvlhigh.setText(Integer.toString(mSound));
+                                timer1.start();
+                            }else {
+                                tv_soundlvlhigh.setText(Integer.toString(mSound) + "...Error 4");
+                                tv_soundlvlhigh.setTextColor(Color.RED);
+                            }
+                        }
+                    });
+                    builder.create().show();
+                }else{
+                    tv_soundlvllow.setText(Integer.toString(mSound)+"...Error 3");
+                    tv_soundlvllow.setTextColor(Color.RED);
+                }
             }
         });
         bt_test2.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
+                timer3.start();
             }
         });
     }
     public void showResultWindow(){
         layoutResutWindow.setVisibility(View.VISIBLE);
+        bt_test1.setTextColor(Color.GRAY);
+        bt_test1.setClickable(false);
+        bt_test2.setTextColor(Color.GRAY);
+        bt_test2.setClickable(false);
     }
     //Notify the download completed for the day (year, month, day) (callback)
     @Override
@@ -219,20 +347,20 @@ public class MainActivity extends AppCompatActivity implements KoalaInterface {
     @Override
     public void connectionStatus(int status, final String message) {
         mConnectStatus = status;
-      //  Log.e("mConnectStatus",""+mConnectStatus+message);
         handler.post(new Runnable(){
             @Override
             public void run() {
                 if (mConnectStatus == 0) {
-                    //btConnect.setText(DISCONNECT);
                     layoutAccessoryWindow.setVisibility(View.GONE);
                     showResultWindow();
                     verifyTask();
-                } else if(mConnectStatus == 1 || mConnectStatus == 2 ) {
+                } else if( mConnectStatus == 2 ) {
                     layoutAccessoryWindow.setVisibility(View.GONE);
-                   // btConnect.setVisibility(View.VISIBLE);
-                } else if(mConnectStatus == 3 ) {
-                  //  btConnect.setText(CONNECTING);
+                    btConnect.setVisibility(View.VISIBLE);
+                } else if(mConnectStatus == 1 ) {
+                    btConnect.setVisibility(View.VISIBLE);
+                    layoutAccessoryWindow.setVisibility(View.GONE);
+                    layoutResutWindow.setVisibility(View.INVISIBLE);
                 }
             }
         });
@@ -242,60 +370,44 @@ public class MainActivity extends AppCompatActivity implements KoalaInterface {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                koalaSDK.requestKoalaTime();
+                koalaSDK.enableLiveUpdate(true);
             }
         },200);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                koalaSDK.requestMcuVersion();
+                mKoalaMcuVersion=koalaSDK.getKoalaMcuVersion();
             }
         },400);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                koalaSDK.requestKoalaBtVersion();
-            }
-        },600);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-            koalaSDK.requestKoalaModel();
+                mKoalaBtVersion=koalaSDK.getKoalaBtVersion();
             }
         },800);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                koalaSDK.enableLiveUpdate(true);
+                mModelName=koalaSDK.getKoalaModel();
             }
         },1000);
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                koalaSDK.updateKoalaTime();
-            }
-        },1200);
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
                 StartTestFlow();
             }
-        }, 5000);
+        }, 7000);
     }
 
     private void StartTestFlow() {
         tv_matname.setText(deviceAdapter.getDeviceName());
-        tv_btaddr.setText(deviceAdapter.getDeviceAddress());
+        tv_btaddr.setText(koalaSDK.getKoalaAddress());
         if(configInfo.mStrMatFwVersion.equals(mKoalaMcuVersion)){
             tv_matfw.setText(mKoalaMcuVersion);
-            if(configInfo.mStrBtFwversion.equals(koalaSDK.getKoalaBtVersion())){
+            if(configInfo.mStrBtFwversion.equals(mKoalaBtVersion)){
                 tv_btfw.setText(mKoalaBtVersion);
-                if(configInfo.mStrMatModelName.equals(koalaSDK.getKoalaModel())){
-                    tv_matmodel.setText(mModelName);
-                }else {
-                    tv_matmodel.setText(mModelName+"...Error 3");
-                    tv_matmodel.setTextColor(Color.RED);
-                }
+                bt_test1.setClickable(true);
+                bt_test1.setTextColor(Color.BLACK);
             }else{
                 tv_btfw.setText(mKoalaBtVersion+"...Error 2");
                 tv_btfw.setTextColor(Color.RED);
@@ -326,7 +438,6 @@ public class MainActivity extends AppCompatActivity implements KoalaInterface {
     @Override
     public void koalaModelName(String modelName) {
         mModelName=modelName;
-        tv_matmodel.setText(modelName);
     }
 
     @Override
